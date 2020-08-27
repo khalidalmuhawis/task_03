42cf66f72abc382499b60ddc19e8f67e824e2c3b
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-08-27 18:58:18 +0300

    task 03

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,9 +4,13 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
     context = {
+    "my_list" : [
+    {"restaurant_name": "Five Guys","food_type": "American Cuisine"},
+    {"restaurant_name": "Forchetta","food_type": "Italian Cuisine"},
+    {"restaurant_name": "Tokyo","food_type": "Japanese Cuisine"}
 
+]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +18,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    "my_object": {
+    "restaurant_name": "Five Guys",
+    "food_type": "American Cuisine"
+    }
 
     }
     return render(request, 'detail.html', context)
